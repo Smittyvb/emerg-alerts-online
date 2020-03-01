fbfdbebe064b02e78606a08a1cd80db89f424c40
Smitty <smitty@vanbodegom.ca>
2020-03-01 08:48:02 -0500

    Improve client

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,37 +1,141 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Platform.Cmd exposing (..)
+import Url
+import Url.Parser as Parser exposing (Parser, oneOf, s, map, top, (</>))
+
 
 -- MAIN
 
+
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
 
 -- MODEL
 
-type alias Alert = {}
-type alias Model = { alerts: List Alert, loading: Bool }
 
-init : Model
-init =
-  { alerts = [], loading: false }
+type alias Alert =
+    {}
 
--- UPDATE
 
-type Msg = Noop
+type ConnectionStatus
+    = Connecting
+    | Connected
+    | Delayed -- heartbeats aren't coming through
+    | Reconnecting
+    | Disconnected
 
-update : Msg -> Model -> Model
+
+type alias Model =
+    { alerts : List Alert
+    , connectionStatus : ConnectionStatus
+    , lastUpdate : Int
+    , key : Nav.Key
+    , url : Url.Url
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( { alerts = []
+      , connectionStatus = Connected
+      , lastUpdate = 0
+      , key = key
+      , url = url
+      }
+    , Cmd.none
+    )
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Noop ->
-      model
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
 
 -- VIEW
 
-view : Model -> Html Msg
-view model =
-  div []
-    [ h1 [] [ text "AlertReady alerts live" ]
+
+viewLink : String -> String -> Html msg
+viewLink name path =
+    a [ href path ] [ text name ]
+
+
+headerEle : Model -> Html Msg
+headerEle model =
+    header []
+        [ h1 []
+            [ text "AlertReady viewer"
+            ]
+        , viewLink "Home" "/"
+        , viewLink "FAQ" "/faq"
+        , viewLink "About" "/about"
+        ]
+
+type Route
+    = Home
+    | About
+    | Faq
+    | SpecificAlert String
+    | NotFound
+
+route : Parser (Route -> a) a
+route =
+  oneOf
+    [ Parser.map Home (Parser.s "home")
+    , Parser.map About (Parser.s "about")
+    , Parser.map Faq (Parser.s "Faq")
+    , Parser.map SpecificAlert (Parser.s "alert" </> Parser.string)
     ]
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "AlertReady viewer"
+    , body =
+        [ div []
+            [ headerEle model
+            
+        , div [] [ text (Url.toString model.url) ]
+        ]]
+    }
