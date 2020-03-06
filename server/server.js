18d064a82a129ec95a5f9299afcc1ef376346552
Smitty <smitty@vanbodegom.ca>
2020-03-05 21:04:41 -0500

    Improve server

--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ const TCP_API_SERVERS = {
   ]
 };
 let alerts = {};
+
+function parseAlertJson(alert) {
+  return {
+    title: Math.random(),
+  }
+}
+
 Object.keys(TCP_API_SERVERS).forEach(key => {
   alerts[key] = [];
   TCP_API_SERVERS[key].forEach(server => {
@@ -30,16 +37,27 @@ Object.keys(TCP_API_SERVERS).forEach(key => {
       pendingXml += dataStr;
       console.log(Date.now(), "got", dataStr.length, "bytes from", server.host + ":" + server.port);
       currentPSP = xml2js.parseStringPromise(pendingXml);
-      let xml;
+      let alert;
       try {
-        xml = await currentPSP;
+        alert = await currentPSP;
       } catch (e) {
         console.log("Incomplete XML");
         return;
       }
+      alert = alert.alert;
+      console.log(alert, parseAlertJson(alert));
+      const id = alert.identifier[0];
+      alerts[id] = {
+        alert: parseAlertJson(alert),
+        rawXml: pendingXml,
+      };
       pendingXml = "";
-      const id = alert.$.identifier[0];
-      alerts[id] = xml;
+      console.log("ID:", id);
+      sseCons.forEach(con => {
+        con.socket.write("\n" + JSON.stringify({
+          alert: alerts[id].alert,
+        }) + "\n")
+      });
     });
     socket.on("connect", () => {
       console.log("Streaming", key, "alerts from", server.host + ":" + server.port);
@@ -52,9 +70,32 @@ Object.keys(TCP_API_SERVERS).forEach(key => {
 });
 
 const app = express();
+let sseCons = [];
 app.get("/api/:feedid/event-stream", (req, res) => {
-
+  // this is called EventStream or Server-Sent Events
+  const id = Math.random();
+  res.status(200).set({
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+    "Content-Type": "text/event-stream"
+  });
+  sseCons.push({
+    res,
+    id,
+  });
+  res.socket.on("close", function() {
+    console.log("sse socket close");
+    sseCons = sseCons.filter(con => con.id !== id);
+  });
+  res.write(": Connected\n");
+  res.write(JSON.stringify(alerts.map(x => {alert: x.alert})) + "\n");
 });
+setInterval(() => {
+  sseCons.forEach(con => {
+    con.res.write(":");
+  });
+}, 8000);
+
 app.get("/api/:feedid/search", (req, res) => {
 
 });
