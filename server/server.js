34e6c84d88d4a20940108aec8bf6acee796245a2
Smitty <smitty@vanbodegom.ca>
2020-03-14 11:34:10 -0400

    Throttle requests less

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ const PER_BACKEND_FUNCS = {
     checkIfAlertSigned: async function(alert) {
 
     },
-    oldAlertQueue: throttledQueue(5, 5000), // at most 5 requests every 5 seconds
+    oldAlertQueue: throttledQueue(4, 1500), // at most 4 requests every 1.5 seconds
     fetchOldAlert: async function(ref, forceBackupServer = false) {
       // page 24 of the LMD guide
       // cap-pac@canada.ca,urn:oid:2.49.0.1.124.4280542342.2020,2020-03-06T14:32:38-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.2271109197.2020,2020-03-06T14:33:38-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.2825789534.2020,2020-03-06T14:32:34-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.2654922216.2020,2020-03-06T14:32:27-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.1308899007.2020,2020-03-06T15:32:42-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.3622084484.2020,2020-03-06T15:33:17-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.2302068752.2020,2020-03-06T15:33:42-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.2627528432.2020,2020-03-06T15:34:17-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.1656974427.2020,2020-03-06T15:53:15-00:00 cap-pac@canada.ca,urn:oid:2.49.0.1.124.2963379165.2020,2020-03-06T15:53:24-00:00
