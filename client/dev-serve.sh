87d306a5198a3b2ae4cb3b761ae14047170e6de8
Smitty <smitty@vanbodegom.ca>
2020-03-14 13:43:18 -0400

    Remove --debug by default
    Slows stuff down too much

--- client/dev-serve.sh
+++ client/dev-serve.sh
@@ -1,4 +1,4 @@
 # relies on serve NPM module being installed globally
 
 echo "Starting dev server"
-serve -s src/ & elm-live --no-server -- src/Main.elm --output=src/elm.js --debug && fg
+serve -s src/ & elm-live --no-server -- src/Main.elm --output=src/elm.js && fg
